f7fceacd151486e4748963c80bf77d7ba5ba2ac1
MShahidKhan <shahidlanger0@gmail.com>
2021-07-14 21:40:48 +0500

    this is lesson no 4 of the crwn clothing projec

--- src/App.js
+++ src/App.js
@@ -3,19 +3,14 @@ import "./App.css";
 import HomePage from "./pages/homepage/homepage.component";
 import "./App.css";
 import { Route, Switch } from "react-router-dom";
-
-const HatePage = () => (
-  <div>
-    <h1>this is hats page</h1>
-  </div>
-);
+import ShopPage from "./pages/shop/shop.component";
 
 function App() {
   return (
     <div className="App">
       <Switch>
         <Route exact path="/" component={HomePage} />
-        <Route path="/hatepage" component={HatePage} />
+        <Route path="/shop" component={ShopPage} />
       </Switch>
     </div>
   );
