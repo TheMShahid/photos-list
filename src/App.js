6a2e80f60cd0d3c736d64afb1525c1b7f9db20b2
MShahidKhan <shahidlanger0@gmail.com>
2021-07-14 15:22:08 +0500

    this is lesson 3 of the crwn clothing project udate our menu-items to navigate with router HOC

--- src/App.js
+++ src/App.js
@@ -2,11 +2,21 @@ import React from "react";
 import "./App.css";
 import HomePage from "./pages/homepage/homepage.component";
 import "./App.css";
+import { Route, Switch } from "react-router-dom";
+
+const HatePage = () => (
+  <div>
+    <h1>this is hats page</h1>
+  </div>
+);
 
 function App() {
   return (
     <div className="App">
-      <HomePage />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/hatepage" component={HatePage} />
+      </Switch>
     </div>
   );
 }
